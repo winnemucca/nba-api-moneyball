3656e9e850d90b70d116ff6bc0dddc1388a498a9
Steven Kauyedauty <skauyedauty@gmail.com>
2019-11-01 12:55:08 -0600

    icons quit working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared-routing-module';
+import { TabsModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
@@ -19,8 +19,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserModule,
     SharedModule,
     AppRoutingModule,
-    MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    TabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
