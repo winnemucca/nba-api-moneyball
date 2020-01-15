bf8bd64530bddb46f275b48ee90984b789850ed2
Steven Kauyedauty <skauyedauty@gmail.com>
2020-01-15 08:59:53 -0700

    moved

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule} from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.
+    forRoot('mongodb+srv://skauyedauty:morbssss@cluster0-l5nf2.mongodb.net/nestjs-demo?retryWrites=true&w=majority'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
